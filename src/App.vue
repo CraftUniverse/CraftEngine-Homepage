d0125d8e1037cfb81d8b6f2c3ece8909d904e36c
Turboman3000 <turboman@turboman.de>
2025-02-20 13:27:21 +0100

    feat(App.vue): add footer with navigation links and copyright

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,38 @@
   <v-app>
     <v-main>
       <router-view />
+      <v-footer app :elevation="16">
+        <v-row justify="center" no-gutters>
+          <v-btn
+            v-for="link in links"
+            :key="link.title"
+            class="mx-2"
+            color="white"
+            rounded="xl"
+            variant="text"
+            :to="link.url.startsWith('https://') ? '' : link.url"
+            :href="link.url.startsWith('https://') ? link.url : ''"
+            :target="link.url.startsWith('https://') ? '_blank' : '_self'"
+          >
+            {{ link.title }}
+          </v-btn>
+          <v-col class="text-center mt-4" cols="12">
+            &#169; {{ new Date().getFullYear() }} —
+            <strong>CraftUniverse</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//@ts-ignore
+import { ref } from "vue";
+
+const links = ref<{ title: string; url: string }[]>([
+  { title: "Home", url: "/" },
+  { title: "Discord", url: "https://rcurl.de/cudc" },
+  { title: "Github", url: "https://github.com/CraftUniverse" },
+]);
 </script>
